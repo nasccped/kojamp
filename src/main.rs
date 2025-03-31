3bfb7ca520a3c771f6b073fa441ad972adf58bdf
NASCC <pedro.main.tec@gmail.com>
2025-03-30 21:57:56 -0300

    [UPDATED] Using the recently created subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+mod commands;
+
+use clap::Command;
+
+const PROGRAM_NAME: &str = "kojamp";
+const CURRENT_VERSION: &str = "0.0.1";
+const AUTHOR: &str = "nasccped <pdbt.contact@gmail.com>";
+const ABOUT: &str = "Some kind of basic 'n academic Java/Kotlin \"project-manager\" tool";
+
 fn main() {
-    println!("Hi there! @nasccped here");
-    println!("Welcome to Kojamp Man. project!");
+    let kojamp_man = Command::new(PROGRAM_NAME)
+        .version(CURRENT_VERSION)
+        .author(AUTHOR)
+        .about(ABOUT)
+        .subcommand(commands::hello::command())
+        .subcommand(commands::bye::command())
+        .get_matches();
+
+    if let Some(refer) = kojamp_man.subcommand_matches("hello") {
+        commands::hello::action(refer);
+    } else if let Some(refer) = kojamp_man.subcommand_matches("bye") {
+        commands::bye::action(refer);
+    } else {
+        println!("No subcommand provided");
+    }
 }
