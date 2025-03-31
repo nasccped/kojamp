4082fd01e7308ef33956415c14b19eb769a26064
NASCC <pedro.main.tec@gmail.com>
2025-03-31 20:24:33 -0300

    [UPDATED] Catching and running the builded CLI from commands mod

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
 mod commands;
+mod kojamp_env;
 
-use clap::Command;
-
-const PROGRAM_NAME: &str = "kojamp";
-const CURRENT_VERSION: &str = "0.0.1";
-const AUTHOR: &str = "nasccped <pdbt.contact@gmail.com>";
-const ABOUT: &str = "Some kind of basic 'n academic Java/Kotlin \"project-manager\" tool";
+use commands::KojampMainCommandUtils;
+use kojamp_env::KojampEnvStruct;
 
 fn main() {
-    let kojamp_man = Command::new(PROGRAM_NAME)
-        .version(CURRENT_VERSION)
-        .author(AUTHOR)
-        .about(ABOUT)
-        .subcommand(commands::hello::command())
-        .subcommand(commands::bye::command())
-        .get_matches();
-
-    if let Some(refer) = kojamp_man.subcommand_matches("hello") {
-        commands::hello::action(refer);
-    } else if let Some(refer) = kojamp_man.subcommand_matches("bye") {
-        commands::bye::action(refer);
-    } else {
-        println!("No subcommand provided");
-    }
+    let envars = KojampEnvStruct::new();
+    let kojamp_man = commands::build_main_command(&envars);
+    kojamp_man.run_kojamp();
 }
