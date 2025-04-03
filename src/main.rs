2f2beae6a7c5c94a0055f1de835eec568b5bb8d6
NASCC <pedro.main.tec@gmail.com>
2025-04-03 00:11:44 -0300

    [UPDATED] Calling updated code on main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-mod commands;
-mod kojamp_env;
+mod kojamp;
+
 pub mod utils;
 
-use commands::KojampMainCommandUtils;
-use kojamp_env::KojampEnvStruct;
-
-const KOJAMP_ENV_VALUES: &str = include_str!("kojamp.env");
-
 fn main() {
-    let kojamp_env_mapping = utils::turn_env_into_hashmap(KOJAMP_ENV_VALUES);
-    let envars = KojampEnvStruct::new(kojamp_env_mapping);
-    let kojamp_man = commands::build_main_command(&envars);
-    kojamp_man.run_kojamp();
+    let main_cli = kojamp::build_kojamp();
+    let _ = main_cli.run();
 }
