66a1b3dac502658caec80c7a6ccfeb2899cca7fc
NASCC <pedro.main.tec@gmail.com>
2025-04-03 17:31:41 -0300

    [UPDATED] Improving main CLI struct + Impl a test situation for returning

--- src/kojamp/kojamp_cli.rs
+++ src/kojamp/kojamp_cli.rs
@@ -8,9 +8,43 @@ pub const PROGRAM_AUTHOR: &str = "nasccped <pdbt.contact@gmail.com>";
 
 pub struct KojampCLI(Command);
 
+use super::KojampOutput;
+
+pub enum TestCase {
+    ItsOk,
+    DefinitelyNotOk,
+}
+
+impl KojampOutput<TestCase, i32> for TestCase {
+    fn new(default: TestCase) -> Self {
+        default
+    }
+    fn update(&mut self, new_value: TestCase) {
+        *self = new_value;
+    }
+
+    fn log_value(&self) {
+        match self {
+            TestCase::ItsOk => println!("Everything is ok!"),
+            TestCase::DefinitelyNotOk => println!("Everything is wrong  Xd"),
+        }
+    }
+
+    fn get_value(&self) -> Result<i32, i32> {
+        match self {
+            TestCase::ItsOk => Ok(0),
+            TestCase::DefinitelyNotOk => Err(1),
+        }
+    }
+}
+
 impl KojampCLI {
     pub fn new(name: &'static str) -> Self {
         KojampCLI(Command::new(name))
+    }
+
+    fn get_inner_value(&self) -> Command {
+        self.0.clone()
     }
 
     pub fn add_version(self, version: &'static str) -> Self {
@@ -30,19 +64,21 @@ impl KojampCLI {
         Self(self.0.subcommand(sub))
     }
 
-    pub fn run(&self) -> KojampOutStatus {
-        let inner = self.0.clone();
-        inner.get_matches();
-        KojampOutStatus(1)
+    pub fn run(&self) -> impl KojampOutput<TestCase, i32> {
+        let _unused_inner = self.get_inner_value();
+        let mut output = TestCase::new(TestCase::ItsOk);
+        println!("Value \x1b[1;32mbefore\x1b[0m update: (0)");
+        output.log_value();
+        output.update(TestCase::DefinitelyNotOk);
+        println!("Value \x1b[1;31mafter\x1b[0m update: (1)");
+        output.log_value();
+        output
     }
-}
 
-#[allow(dead_code)]
-pub struct KojampOutStatus(u8);
-
-#[allow(dead_code)]
-impl KojampOutStatus {
-    pub fn get_value(&self) -> u8 {
-        self.0
+    pub fn return_final_output(&self, output: impl KojampOutput<TestCase, i32>) {
+        match output.get_value() {
+            Ok(_) => std::process::exit(0),
+            Err(_) => std::process::exit(1),
+        }
     }
 }
