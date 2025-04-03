6f44f9429b60df9b14ce0ee8a2698585c7bb44a1
NASCC <pedro.main.tec@gmail.com>
2025-04-03 17:32:08 -0300

    [UPDATED] Integrate changes to module

--- src/kojamp/mod.rs
+++ src/kojamp/mod.rs
@@ -1,5 +1,8 @@
 mod kojamp_cli;
+mod kojamp_output;
+
 use kojamp_cli::{KojampCLI, PROGRAM_ABOUT, PROGRAM_AUTHOR, PROGRAM_NAME, PROGRAM_VERSION};
+pub use kojamp_output::KojampOutput;
 
 pub fn build_kojamp() -> KojampCLI {
     KojampCLI::new(PROGRAM_NAME)
