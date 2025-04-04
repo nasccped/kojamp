23284796d61b60717e15a8044dc619666b2b8ef4
NASCC <pedro.main.tec@gmail.com>
2025-04-04 13:03:11 -0300

    [UPDATED] Calling chages os main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ pub mod utils;
 
 fn main() {
     let main_cli = kojamp::build_kojamp();
-    let out = main_cli.run();
-    main_cli.return_final_output(out);
+    let action = main_cli.get_action();
+    let out = main_cli.run(action);
+    main_cli.exit_with_output(out);
 }
