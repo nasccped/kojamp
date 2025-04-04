ff38336077cd3033613f191eb0637f560bcb4469
NASCC <pedro.main.tec@gmail.com>
2025-04-04 14:51:27 -0300

    [UPDATED] Impl all changes to project call

--- src/kojamp/mod.rs
+++ src/kojamp/mod.rs
@@ -1,11 +1,14 @@
 mod kojamp_cli;
 mod kojamp_output;
 
-use kojamp_cli::{KojampCLI, PROGRAM_ABOUT, PROGRAM_AUTHOR, PROGRAM_NAME, PROGRAM_VERSION};
+use kojamp_cli::{
+    gen_default_style, KojampCLI, PROGRAM_ABOUT, PROGRAM_AUTHOR, PROGRAM_NAME, PROGRAM_VERSION,
+};
 pub use kojamp_output::KojampOutput;
 
 pub fn build_kojamp() -> KojampCLI {
     KojampCLI::new(PROGRAM_NAME)
+        .add_styles(gen_default_style())
         .add_version(PROGRAM_VERSION)
         .add_author(PROGRAM_AUTHOR)
         .add_about(PROGRAM_ABOUT)
