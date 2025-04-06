25ce76750f4324849b394e83707668c9e8d5cb05
NASCC <pedro.main.tec@gmail.com>
2025-04-06 11:26:08 -0300

    [REMOVED] Delete useless return trait

--- src/kojamp/kojamp_output.rs
+++
@@ -1,6 +0,0 @@
-pub trait KojampOutput<ImplType, ReturnType> {
-    fn new(default: ImplType) -> ImplType;
-    fn update(&mut self, new_value: ImplType);
-    fn log_value(&self);
-    fn get_value(&self) -> Result<ReturnType, ReturnType>;
-}
