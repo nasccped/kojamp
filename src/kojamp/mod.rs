2d74afed89dd5336b31d0111e4204193818adeba
NASCC <pedro.main.tec@gmail.com>
2025-04-06 10:22:32 -0300

    [UPDATED] Insert 'new' command into CLI

--- src/kojamp/mod.rs
+++ src/kojamp/mod.rs
@@ -1,15 +1,25 @@
 mod kojamp_cli;
+mod kojamp_commands;
 mod kojamp_output;
 
 use kojamp_cli::{
     gen_default_style, KojampCLI, PROGRAM_ABOUT, PROGRAM_AUTHOR, PROGRAM_NAME, PROGRAM_VERSION,
 };
+use kojamp_commands as kjc;
 pub use kojamp_output::KojampOutput;
 
 pub fn build_kojamp() -> KojampCLI {
-    KojampCLI::new(PROGRAM_NAME)
+    let subcommands = [(kjc::command_new(), kjc::action_new)];
+
+    let mut app = KojampCLI::new(PROGRAM_NAME)
         .add_styles(gen_default_style())
         .add_version(PROGRAM_VERSION)
         .add_author(PROGRAM_AUTHOR)
-        .add_about(PROGRAM_ABOUT)
+        .add_about(PROGRAM_ABOUT);
+
+    for sub in subcommands {
+        app = app.add_subcommand(sub.0);
+    }
+
+    app
 }
