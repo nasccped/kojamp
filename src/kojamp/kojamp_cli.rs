309313ef8117bcb214f8315e599f68e4b642b5ef
NASCC <pedro.main.tec@gmail.com>
2025-04-07 16:08:21 -0300

    [UPDATED] Removing useless functions + handle help first

--- src/kojamp/kojamp_cli.rs
+++ src/kojamp/kojamp_cli.rs
@@ -1,7 +1,6 @@
 use clap::{
     builder::{styling, Styles},
-    error::ErrorKind,
-    ArgMatches, Command, Error as ClapError,
+    Command,
 };
 
 use super::kjc;
@@ -9,7 +8,7 @@ use super::kjc;
 pub const PROGRAM_NAME: &str = "kojamp";
 pub const PROGRAM_VERSION: &str = "0.0.1";
 pub const PROGRAM_ABOUT: &str =
-    "Some kind of basic 'n academic \x1b[1;31mJava \x1b[37m/ \x1b[33mKotlin\x1b[0m \"project-manager\" tool";
+    "Some kind of basic 'n academic \x1b[1;31mJava\x1b[37m/\x1b[33mKotlin\x1b[0m \"project-manager\" tool";
 pub const PROGRAM_AUTHOR: &str = "nasccped, pdbt.contact@gmail.com";
 
 pub struct KojampCLI(Command);
@@ -24,7 +23,9 @@ pub fn gen_default_style() -> Styles {
 
 impl KojampCLI {
     pub fn new(name: &'static str) -> Self {
-        KojampCLI(Command::new(name))
+        let mut command = Command::new(name);
+        command.set_bin_name(PROGRAM_NAME);
+        KojampCLI(command)
     }
 
     fn get_inner_value(&self) -> Command {
@@ -47,57 +48,25 @@ impl KojampCLI {
         Self(self.0.about(about))
     }
 
-    #[allow(dead_code)]
-    pub fn add_subcommand(self, sub: Command) -> Self {
-        Self(self.0.subcommand(sub))
+    pub fn add_subcommand(self, sub: Command, style: Styles) -> Self {
+        Self(self.0.subcommand(sub.styles(style)))
     }
 
-    fn unexpected_input(&self) {
-        println!("The given input(s) \x1b[1;31mcouldn't\x1b[0m be used!");
-        println!("Probably due to invalid input format, or undefined subcommand...");
-        println!("Consider using \x1b[1;36m`kojamp --help`\x1b[0m");
-    }
-
-    fn print_help(&self) {
-        let mut inner = self.get_inner_value();
-        let _ = inner.print_help();
-    }
-
-    fn print_version(&self) {
-        let inner = self.get_inner_value();
-        let version = inner.get_version();
-        println!("kojamp {}", version.unwrap());
-    }
-
-    pub fn get_action(&self) -> Result<ArgMatches, ClapError> {
-        let inner = self.get_inner_value();
-        inner.try_get_matches()
-    }
-
-    pub fn run(&self, action: Result<ArgMatches, ClapError>) -> i32 {
+    pub fn run(&self) -> i32 {
         let mut output = 0;
 
-        if let Err(x) = action {
-            match x.kind() {
-                ErrorKind::DisplayHelp => self.print_help(),
-                ErrorKind::DisplayVersion => self.print_version(),
-                _ => {
-                    self.unexpected_input();
-                    output += 1
-                }
-            }
-            return output;
-        }
+        let mut app = self.get_inner_value();
 
+        // if no args given
         if std::env::args().count() == 1 {
-            self.print_help();
-            return output;
+            let _ = app.print_help().unwrap();
+            return 0;
         }
 
-        let action = action.unwrap();
+        let matches = app.get_matches();
 
-        match action.subcommand().unwrap() {
-            ("new", new_matching) => {
+        match matches.subcommand() {
+            Some(("new", new_matching)) => {
                 output = kjc::action_new(new_matching);
             }
             _ => {
