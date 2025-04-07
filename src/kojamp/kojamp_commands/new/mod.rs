a8ff1ebce130a97666d8aa1c409aa8b1e1bef94e
NASCC <pedro.main.tec@gmail.com>
2025-04-07 16:14:11 -0300

    [UPDATED] Args added + args catching

--- src/kojamp/kojamp_commands/new/mod.rs
+++ src/kojamp/kojamp_commands/new/mod.rs
@@ -1,13 +1,43 @@
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgAction, ArgMatches, Command};
 
 const COMMAND_NAME: &str = "new";
-const COMMAND_ABOUT: &str = "Create a new \x1b[1;31mJava \x1b[37m/ \x1b[33mKotlin\x1b[0m project";
+const COMMAND_ABOUT: &str = "Create a new Java/Kotlin project";
 
 pub fn command_new() -> Command {
-    Command::new(COMMAND_NAME).about(COMMAND_ABOUT)
+    Command::new(COMMAND_NAME).about(COMMAND_ABOUT).args([
+        Arg::new("name")
+            .value_name("NAME")
+            .action(ArgAction::Set)
+            .required(false)
+            .help("Specify the project's name (numeric starting isn't allowed)")
+            .index(1),
+        Arg::new("path")
+            .value_name("PATH")
+            .action(ArgAction::Set)
+            .long("path")
+            .short('P')
+            .required(false)
+            .help("Specify the project's path (self name in kebab case as default)"),
+        Arg::new("author")
+            .value_name("AUTHOR")
+            .action(ArgAction::Set)
+            .long("author")
+            .short('A')
+            .required(false)
+            .help("Specify the project's author(s) (comma separated)")
+            .num_args(0..),
+    ])
 }
 
-pub fn action_new(_mtc: &ArgMatches) -> i32 {
-    println!("This is printed when `new` command is called!");
+pub fn action_new(mtc: &ArgMatches) -> i32 {
+    let (name, path, author): (_, _, Option<Vec<&String>>) = (
+        mtc.get_one::<String>("name"),
+        mtc.get_one::<String>("path"),
+        mtc.get_many::<String>("author")
+            .map(|vals| vals.clone().collect()),
+    );
+
+    println!("{:?}, {:?}, {:?}", name, path, author);
+
     0
 }
