c1da1c3f8b1ab13ccdfdd840d5f3dab6279c33ba
NASCC <pedro.main.tec@gmail.com>
2025-04-07 16:21:59 -0300

    [DELETED] Remove utils for now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod kojamp;
 
-pub mod utils;
-
 fn main() {
     let main_cli = kojamp::build_kojamp();
     let out = main_cli.run();
--- src/utils/mod.rs
+++
@@ -1,3 +0,0 @@
-mod mapping;
-
-pub use mapping::turn_env_into_hashmap;
