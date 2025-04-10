4ee5e0a02a75d61986a9d10167b4314e10a56b1a
NASCC <pedro.main.tec@gmail.com>
2025-04-10 16:08:25 -0300

    [UPDATED] Help fixes + control flow testing

--- src/kojamp/kojamp_commands/new/mod.rs
+++ src/kojamp/kojamp_commands/new/mod.rs
@@ -1,4 +1,7 @@
 use clap::{Arg, ArgAction, ArgMatches, Command};
+mod project;
+
+use project::Project;
 
 const COMMAND_NAME: &str = "new";
 const COMMAND_ABOUT: &str = "Create a new Java/Kotlin project";
@@ -19,7 +22,7 @@ pub fn command_new() -> Command {
             .required(false)
             .help("Specify the project's path (\x1b[93m[NAME]\x1b[0m in kebab case as default)"),
         Arg::new("type")
-            .value_name("(J)ava|(K)otlin ")
+            .value_name("(J)ava|(K)otlin")
             .action(ArgAction::Set)
             .long("type")
             .short('T')
@@ -31,11 +34,11 @@ pub fn command_new() -> Command {
             .long("authors")
             .short('A')
             .required(false)
-            .help("Specify the project's author(s) (comma separated)")
+            .help("Specify the project's author(s) (slash '/' separated)")
             .num_args(1..),
         Arg::new("no-git")
             .long("no-git")
-            .action(ArgAction::SetFalse)
+            .action(ArgAction::SetTrue)
             .required(false)
             .help("Disable git repository initialization"),
         Arg::new("prompt")
@@ -47,46 +50,17 @@ pub fn command_new() -> Command {
 }
 
 pub fn action_new(mtc: &ArgMatches) -> i32 {
-    let (name, path, project_type, authors, git_repo, prompt): (
-        Option<String>,
-        Option<String>,
-        Option<String>,
-        Option<Vec<String>>,
-        bool,
-        bool,
-    ) = (
-        mtc.get_one("name").cloned(),
-        mtc.get_one("path").cloned(),
-        mtc.get_one("type").cloned(),
-        mtc.get_many("authors").map(|vals| vals.cloned().collect()),
-        match mtc.get_one::<bool>("no-git") {
-            Some(&g) => g,
-            _ => true,
-        },
-        match mtc.get_one::<bool>("prompt") {
-            Some(&p) => p,
-            _ => false,
-        },
-    );
+    let mut project = Project::from_match(mtc);
 
-    let prompt_conditions = [
-        name.is_none(),
-        path.is_none(),
-        project_type.is_none(),
-        authors.is_none(),
-        git_repo,
-    ];
-
-    if prompt && !prompt_conditions.into_iter().all(|c| c) {
-        println!("You \x1b[91mcan't\x1b[0m use `\x1b[93mkojamp new --prompt\x1b[0m` with other flags/args!");
-        println!();
-        println!("Try using one of the following:");
-        println!("1) \x1b[92mkojamp new \x1b[93m[NAME]\x1b[0m + other flags (except --prompt)");
-        println!("2) \x1b[92mkojamp new \x1b[93m--prompt\x1b[0m");
-        println!();
-        println!("Use `\x1b[92mkojamp new \x1b[93m--help\x1b[0m` for more details!");
+    if !project.prompt_allowed() && project.prompt_called() {
+        println!("error");
         return 1;
+    } else if project.prompt_called() {
+        project.prompt_mode();
+        return 0;
     }
+
+    println!("{:?}", project);
 
     0
 }
