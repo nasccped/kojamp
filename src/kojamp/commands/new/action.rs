e691ee488601a5977af4d761f4d612ebb79c36e8
NASCC <pedro.main.tec@gmail.com>
2025-04-11 17:49:04 -0300

    [TEMP-FIX] Remove unused function

--- src/kojamp/commands/new/action.rs
+++ src/kojamp/commands/new/action.rs
@@ -2,13 +2,12 @@ use super::project::Project;
 use clap::ArgMatches;
 
 pub fn action(mtc: &ArgMatches) -> i32 {
-    let mut project = Project::from_match(mtc);
+    let project = Project::from_match(mtc);
 
     if !project.prompt_allowed() && project.prompt_called() {
         println!("error");
         return 1;
     } else if project.prompt_called() {
-        project.prompt_mode();
         return 0;
     }
 
