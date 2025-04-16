88b05404ccefcdf2a7f0c220f3a7adeb49aceeb1
NASCC <pedro.main.tec@gmail.com>
2025-04-16 15:43:23 -0300

    [ADDED] Provisional reports for failure scenarios

---
+++ src/kojamp/commands/new/report.rs
@@ -0,0 +1,15 @@
+pub fn prompt_not_allowed() {
+    println!("The prompt isn't allowed");
+}
+
+pub fn invalid_name() {
+    println!("The current name is invalid");
+}
+
+pub fn invalid_project_type() {
+    println!("The current project type is invalid");
+}
+
+pub fn invalid_authors() {
+    println!("The current authors are invalid");
+}
