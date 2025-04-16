38e0216500f922a17769e0bdcd50bf3b61d7e471
NASCC <pedro.main.tec@gmail.com>
2025-04-15 22:13:19 -0300

    [UPDATED] Better user input + new feature (remove lines)

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -1,14 +1,24 @@
-use std::io;
+use std::{
+    fmt,
+    io::{self, Write},
+};
 
-pub fn read_line() -> String {
+pub fn remove_lines(go_up: usize) {
+    print!("\x1B[{}A", go_up);
+    print!("\x1B[0J");
+}
+
+pub fn input<T: fmt::Display>(prompt: T) -> String {
     let mut buffer = String::new();
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
     if let Err(_) = io::stdin().read_line(&mut buffer) {
         "".to_string()
     } else {
-        normalize_read_line_input(buffer)
+        normalize_input(buffer)
     }
 }
 
-pub fn normalize_read_line_input(input: String) -> String {
+pub fn normalize_input(input: String) -> String {
     input.replace("\n", "").replace("\r", "")
 }
