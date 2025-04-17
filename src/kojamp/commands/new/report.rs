dab362d669caf6db58d1498859002ed5088b9462
NASCC <pedro.main.tec@gmail.com>
2025-04-17 11:21:06 -0300

    [FIXED] Passing verbose flag as argument

--- src/kojamp/commands/new/report.rs
+++ src/kojamp/commands/new/report.rs
@@ -2,14 +2,14 @@ pub fn prompt_not_allowed() {
     println!("The prompt isn't allowed");
 }
 
-pub fn invalid_name() {
+pub fn invalid_name(verbose: bool) {
     println!("The current name is invalid");
 }
 
-pub fn invalid_project_type() {
+pub fn invalid_project_type(verbose: bool) {
     println!("The current project type is invalid");
 }
 
-pub fn invalid_authors() {
+pub fn invalid_authors(verbose: bool) {
     println!("The current authors are invalid");
 }
