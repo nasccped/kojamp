e86e1075a2f43d951edf9600f9d11c8623ff65bd
NASCC <pedro.main.tec@gmail.com>
2025-04-19 09:24:31 -0300

    [UPDATED] New check (no args founded)

--- src/kojamp/commands/new/helper.rs
+++ src/kojamp/commands/new/helper.rs
@@ -19,3 +19,19 @@ pub fn only_prompt_called(matches: &ArgMatches) -> bool {
     .iter()
     .all(|cond| *cond)
 }
+
+pub fn no_args_or_flags(matches: &ArgMatches) -> bool {
+    [
+        !prompt_called(matches),
+        matches.get_one::<String>("name").is_none(),
+        matches.get_one::<String>("path").is_none(),
+        matches.get_one::<String>("type").is_none(),
+        matches
+            .get_many("authors")
+            .map(|vector| vector.cloned().collect::<Vec<String>>())
+            .is_none(),
+        !matches.get_flag("no-git"),
+    ]
+    .iter()
+    .all(|cond| *cond)
+}
