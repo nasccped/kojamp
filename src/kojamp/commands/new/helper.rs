745ee9b1301efb022b46e160ce9ed3298fd92fae
NASCC <pedro.main.tec@gmail.com>
2025-04-20 11:36:21 -0300

    [UPDATED] New feature (create a directory)

--- src/kojamp/commands/new/helper.rs
+++ src/kojamp/commands/new/helper.rs
@@ -1,4 +1,5 @@
 use clap::ArgMatches;
+use std::{fs, path::PathBuf};
 
 pub fn prompt_called(matches: &ArgMatches) -> bool {
     matches.get_flag("prompt")
@@ -34,4 +35,43 @@ pub fn no_args_or_flags(matches: &ArgMatches) -> bool {
     ]
     .iter()
     .all(|cond| *cond)
+}
+
+pub enum PathCreationErrors {
+    InvalidPathName,
+    AlreadyExists,
+    ParentDoesntExists,
+    #[allow(dead_code)]
+    PermissionDenied,
+    UndefinedReason,
+}
+
+pub fn create_path(path: PathBuf) -> Result<(), PathCreationErrors> {
+    if path.file_name().is_none() {
+        return Err(PathCreationErrors::InvalidPathName);
+    }
+
+    if path.exists() {
+        return Err(PathCreationErrors::AlreadyExists);
+    }
+
+    if let Some(parent) = path.parent() {
+        if !parent.exists() {
+            return Err(PathCreationErrors::ParentDoesntExists);
+        }
+    }
+
+    #[cfg(unix)]
+    {
+        use std::os::unix::fs::PermissionsExt;
+        let metadata = parent.metadata()?;
+        if metadata.permissions().mode() & 0o200 == 0 {
+            return Err(PathCreationErrors::PermissionDenied);
+        }
+    }
+
+    match fs::create_dir(path) {
+        Ok(_) => Ok(()),
+        Err(_) => Err(PathCreationErrors::UndefinedReason),
+    }
 }
