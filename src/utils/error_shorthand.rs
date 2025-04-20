96981b40cf6f9124dfe719a9bbb33fb208a67108
NASCC <pedro.main.tec@gmail.com>
2025-04-20 11:05:43 -0300

    [FIXED] Remove some utils (no longer necessary)

--- src/utils/error_shorthand.rs
+++
@@ -1,8 +0,0 @@
-use std::borrow::Cow;
-use std::io::{Error, ErrorKind};
-
-pub fn new_error<'a>(error_kind: ErrorKind, message: impl Into<Cow<'a, str>>) -> Error {
-    let message = message.into();
-
-    Error::new(error_kind, message)
-}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,5 @@
 pub mod arg_test;
 pub mod boxed;
-pub mod error_shorthand;
-pub mod fs;
 pub mod io;
 pub mod strings;
 
