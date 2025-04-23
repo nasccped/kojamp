422618a4886dc6af3d9820cf8db0aa9a08f22092
NASCC <pedro.main.tec@gmail.com>
2025-04-23 18:38:30 -0300

    [UPDATED] forking flux (new or init)

--- src/kojamp/action_kind/new_project.rs
+++ src/kojamp/action_kind/new_project.rs
@@ -2,8 +2,19 @@ use clap::ArgMatches;
 
 pub fn main(pair: (&str, ArgMatches)) -> i32 {
     match pair {
-        ("new", _) => println!("Creating a project on a new directory"),
-        _ => println!("Creating a project on the current directory"),
+        ("new", matching) => from_new(matching),
+        (_, matching) => from_init(matching),
     }
+}
+
+fn from_new(matching: ArgMatches) -> i32 {
+    let name: Option<&String> = matching.get_one("name");
+    println!("The project name is: {:?}", name);
+    0
+}
+
+fn from_init(matching: ArgMatches) -> i32 {
+    let name: Option<&String> = matching.get_one("name");
+    println!("The project name is: {:?}", name);
     0
 }
