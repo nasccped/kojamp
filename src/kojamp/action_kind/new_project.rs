a1e17f4a46d13ffc0067e65b35689c543ce443db
NASCC <pedro.main.tec@gmail.com>
2025-04-23 19:19:05 -0300

    [UPDATED] Test path catching + printing show

--- src/kojamp/action_kind/new_project.rs
+++ src/kojamp/action_kind/new_project.rs
@@ -1,20 +1,50 @@
+use crate::utils::report;
 use clap::ArgMatches;
+use colored::Colorize;
+use std::{env, path::PathBuf};
 
-pub fn main(pair: (&str, ArgMatches)) -> i32 {
-    match pair {
-        ("new", matching) => from_new(matching),
-        (_, matching) => from_init(matching),
+struct ProjectName<'a>(&'a String);
+
+impl<'a> ProjectName<'a> {
+    fn new(matching: &'a ArgMatches) -> Self {
+        // NOTE: We can unwrap the name 'cause it's a required
+        //       argument, so will never fail
+        Self(matching.get_one("name").unwrap())
     }
 }
 
-fn from_new(matching: ArgMatches) -> i32 {
-    let name: Option<&String> = matching.get_one("name");
-    println!("The project name is: {:?}", name);
+pub fn main(pair: (&str, ArgMatches)) -> i32 {
+    let cur_path = match env::current_dir() {
+        Err(_) => {
+            report::path::undefined_cur_dir();
+            return 1;
+        }
+        Ok(p) => p,
+    };
+
+    match pair {
+        ("new", matching) => from_new(matching, cur_path),
+        (_, matching) => from_init(matching, cur_path),
+    }
+}
+
+fn from_new(matching: ArgMatches, cur_path: PathBuf) -> i32 {
+    let name = ProjectName::new(&matching);
+    let path = cur_path.join(name.0);
+    println!("Creating the `{}` project on a new", name.0.bright_green());
+    println!("directory: `{}`", path.to_str().unwrap().bright_yellow());
     0
 }
 
-fn from_init(matching: ArgMatches) -> i32 {
-    let name: Option<&String> = matching.get_one("name");
-    println!("The project name is: {:?}", name);
+fn from_init(matching: ArgMatches, cur_path: PathBuf) -> i32 {
+    let name = ProjectName::new(&matching);
+    println!(
+        "Creating the `{}` project on the current",
+        name.0.bright_green()
+    );
+    println!(
+        "directory: `{}`",
+        cur_path.to_str().unwrap().bright_yellow()
+    );
     0
 }
