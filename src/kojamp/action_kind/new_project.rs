efe6163dbb8bc5af09c995b555f0b28175749926
NASCC <pedro.main.tec@gmail.com>
2025-04-23 20:06:21 -0300

    [UPDATED] New implementation to get path

--- src/kojamp/action_kind/new_project.rs
+++ src/kojamp/action_kind/new_project.rs
@@ -8,8 +8,30 @@ struct ProjectName<'a>(&'a String);
 impl<'a> ProjectName<'a> {
     fn new(matching: &'a ArgMatches) -> Self {
         // NOTE: We can unwrap the name 'cause it's a required
-        //       argument, so will never fail
-        Self(matching.get_one("name").unwrap())
+        // argument, so will never fail
+        Self(matching.get_one::<String>("name").unwrap())
+    }
+}
+
+struct ProjectPath(PathBuf);
+
+impl ProjectPath {
+    fn from_path_buf(path_buf: PathBuf) -> Self {
+        Self(path_buf)
+    }
+
+    fn add_from_matching<'a>(&mut self, matching: &'a ArgMatches) -> Option<&'a String> {
+        match matching.get_one("path") {
+            Some(x) => {
+                self.0 = self.0.join(x);
+                Some(x)
+            }
+            _ => None,
+        }
+    }
+
+    fn add_from_project_name<'a>(&mut self, project_name: &'a ProjectName) {
+        self.0 = self.0.join(project_name.0.to_kebab_case());
     }
 }
 
@@ -30,21 +52,23 @@ pub fn main(pair: (&str, ArgMatches)) -> i32 {
 
 fn from_new(matching: ArgMatches, cur_path: PathBuf) -> i32 {
     let name = ProjectName::new(&matching);
-    let path = cur_path.join(name.0.to_kebab_case());
+    let mut path = ProjectPath::from_path_buf(cur_path);
+    if path.add_from_matching(&matching).is_none() {
+        path.add_from_project_name(&name);
+    }
+
     println!("Creating the `{}` project on a new", name.0.bright_green());
-    println!("directory: `{}`", path.to_str().unwrap().bright_yellow());
+    println!("directory: `{}`", path.0.to_str().unwrap().bright_yellow());
     0
 }
 
 fn from_init(matching: ArgMatches, cur_path: PathBuf) -> i32 {
     let name = ProjectName::new(&matching);
+    let path = ProjectPath::from_path_buf(cur_path);
     println!(
         "Creating the `{}` project on the current",
         name.0.bright_green()
     );
-    println!(
-        "directory: `{}`",
-        cur_path.to_str().unwrap().bright_yellow()
-    );
+    println!("directory: `{}`", path.0.to_str().unwrap().bright_yellow());
     0
 }
