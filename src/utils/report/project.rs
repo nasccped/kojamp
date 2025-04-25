939e27719d7a3721e38af2527922980f2944513c
NASCC <pedro.main.tec@gmail.com>
2025-04-25 16:42:57 -0300

    [UPDATED] New report message (project kind is invalid)

--- src/utils/report/project.rs
+++ src/utils/report/project.rs
@@ -32,3 +32,44 @@ pub fn name_is_invalid() {
         "(`--path` flag only available for the `new` subcommand)".italic()
     )
 }
+
+pub fn kind_is_invalid(kind_value: &str) {
+    report_header(
+        ERROR_BADGE.bright_red().bold(),
+        "Couldn't create a new project due to invalid project kind!",
+    );
+    println!();
+
+    println!("This is a Java/Kotlin project manager, so these");
+    println!(
+        "kinds are expected, but {} was specified!",
+        match kind_value {
+            "" => format!("{}", "no kind".bright_red()),
+            x => format!("`{}` kind", x.bright_red()),
+        }
+    );
+    println!();
+
+    println!("You can specify a project kind by");
+    println!(
+        "using the `{} {}`",
+        "--kind".bright_green(),
+        "<PROJECT_KIND>".bright_yellow()
+    );
+    println!();
+
+    println!(
+        "{} {}",
+        "NOTE:".bright_cyan().italic(),
+        format!(
+            "{} {}{}{}{}{} {}",
+            "<PROJECT_KIND>".bright_yellow().italic(),
+            "can only be `".italic(),
+            "Java".bright_blue().italic(),
+            "` or `".italic(),
+            "Kotlin".bright_blue().italic(),
+            "`".italic(),
+            "(No case sensitive)".bright_black().italic()
+        )
+    );
+}
