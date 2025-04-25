ee8f266e9980c505f4b25ce9dc2312d61635f648
NASCC <pedro.main.tec@gmail.com>
2025-04-25 16:09:39 -0300

    [UPDATED] New function for report heading when neccessary

--- src/utils/report/mod.rs
+++ src/utils/report/mod.rs
@@ -1,1 +1,18 @@
 pub mod path;
+pub mod project;
+
+use colored::Colorize;
+use core::fmt;
+
+fn report_header<T, U>(tag: T, text_message: U)
+where
+    T: Colorize + fmt::Display,
+    U: AsRef<str>,
+{
+    let msg = text_message.as_ref();
+    println!(
+        "{tag}{} {}",
+        ":".bright_white().bold(),
+        msg.bright_white().bold()
+    );
+}
