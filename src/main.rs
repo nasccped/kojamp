52256f37c5b32e8caecc711824fd32f5454479e2
NASCC <pedro.main.tec@gmail.com>
2025-04-29 15:43:58 -0300

    [UPDATED] Update main file due to the 'kojamp/' changes

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod globals;
 mod kojamp;
 mod utils;
 
-use kojamp::{kojamp_app, KojampCLI};
+use kojamp::{kojamp_app, KojampApp};
 
 fn main() {
     let mut app = kojamp_app();
     let matching = app.get_matching();
-    let output = app.run_app(matching);
+    let output = app.run_kojamp_app(matching);
     app.exit_output(output);
 }
