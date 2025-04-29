4f6ef6bc09e1d4ce356551b28719d8c1cfc9f38c
NASCC <pedro.main.tec@gmail.com>
2025-04-29 17:41:40 -0300

    [UPDATED] New importings + directory/file creation scripting

--- src/kojamp/action_kind/new_project/init.rs
+++ src/kojamp/action_kind/new_project/init.rs
@@ -1,7 +1,55 @@
-use super::project_fields::ProjectFields;
-use std::io::Error;
+use super::{file_content, project_fields::ProjectFields, project_kind::ProjectKind};
+use crate::utils::array::ToText;
+use std::{
+    fs,
+    io::{Error, ErrorKind},
+};
 
-pub fn init(_project_fields: ProjectFields) -> Result<(), Error> {
-    println!("Let's build this project!");
+const COULD_NOT_CREATE_SRC_DIR: [&str; 2] = [
+    "Couldn't create src directory!",
+    "The reason can be due to memory issue.",
+];
+
+const COULD_NOT_CREATE_MAIN_FILE: [&str; 3] = [
+    "Couldn't create the main file &&",
+    "\x1b[97m(\x1b[91m.java\x1b[97m/\x1b[91m.kt\x1b[97m)\x1b[0m!",
+    "The reason can be due to memory issue.",
+];
+
+pub fn init(project_fields: ProjectFields) -> Result<(), Error> {
+    let name = project_fields.get_name().get_inner();
+    let kind = project_fields.get_kind();
+    let mut path = project_fields.get_path().get_inner();
+    let content: String;
+
+    path.push("src");
+    if fs::create_dir(&path).is_err() {
+        return Err(Error::new(
+            ErrorKind::Other,
+            COULD_NOT_CREATE_SRC_DIR.to_text(),
+        ));
+    }
+
+    path.push(name);
+    path.set_extension(match kind {
+        ProjectKind::Java => {
+            content = file_content::java(name);
+            "java"
+        }
+        _ => {
+            content = file_content::kotlin(name);
+            "kt"
+        }
+    });
+
+    if fs::write(path, content).is_err() {
+        return Err(Error::new(
+            ErrorKind::Other,
+            COULD_NOT_CREATE_MAIN_FILE.to_text(),
+        ));
+    }
+
+    // TODO: impl the remain of the code (Kojamp.toml, README.md, .gitignore)
+
     Ok(())
 }
