04c2975020ca38b4ea938b1b154a839244f0cf4c
NASCC <pedro.main.tec@gmail.com>
2025-04-30 18:00:11 -0300

    [UPDATED] New global values for file/dir creation

--- src/globals.rs
+++ src/globals.rs
@@ -13,3 +13,15 @@ pub const PROGRAM_REPO_URL: &str = "https://github.com/nasccped/kojamp";
 
 pub const SUCCESS_EXIT_STATUS: i32 = 0;
 pub const FAILURE_EXIT_STATUS: i32 = 1;
+
+pub const SRC_DIR: &str = "src";
+pub const JAVA_FILE_EXTENSION: &str = "java";
+pub const KOTLIN_FILE_EXTENSION: &str = "kt";
+pub const PROGRAM_TOML_FILE_NAME: &str = "Kojamp";
+pub const TOML_FILE_EXTENSION: &str = "toml";
+pub const README_FILE_NAME: &str = "README";
+pub const MARKDOWN_FILE_EXTENSION: &str = "md";
+
+pub const GIT_COMMAND: &str = "git";
+pub const GIT_INITIALIZATION_ARG: &str = "init";
+pub const GIT_IGNORE_FILE_FULLNAME: &str = ".gitignore";
