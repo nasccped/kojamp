2167d0974383f7ac4f4946707866260a213ea16d
NASCC <pedro.main.tec@gmail.com>
2025-04-30 14:13:29 -0300

    [UPDATED] New file creation (README.md)

--- src/kojamp/action_kind/new_project/file_content.rs
+++ src/kojamp/action_kind/new_project/file_content.rs
@@ -1,4 +1,6 @@
-use crate::globals::PROGRAM_REPO_URL;
+use super::ProjectKind;
+use crate::globals::{PROGRAM_REPO_URL, PROGRAM_VERSION};
+use std::rc::Rc;
 
 pub fn java(class_name: &str) -> String {
     [
@@ -81,4 +83,49 @@ pub fn kotlin(file_name: &str) -> String {
         "}",
     ]
     .join("\n")
+}
+
+pub fn readme(
+    project_name: &str,
+    project_kind: &ProjectKind,
+    project_authors: Option<Vec<Rc<str>>>,
+) -> String {
+    let mut begin = vec![
+        String::from("<div align=\"center\">"),
+        String::new(),
+        format!("{}", project_name),
+        format!("{}", "=".repeat(project_name.len())),
+        String::new(),
+        format!("[![built in](https://img.shields.io/badge/built_in-kojamp_{}-blue?)](https://github.com/nasccped/kojamp)", PROGRAM_VERSION),
+        format!(
+            "[![project kind](https://img.shields.io/badge/project_kind-{}?)](#)", match project_kind {
+                ProjectKind::Java => "java-orange",
+                _ => "kotlin-blue",
+            }
+        ),
+        String::new(),
+        String::from("</div>"),
+        String::new(),
+        format!(
+            "Welcome to the **{}** project{}",
+            project_name,
+            if project_authors.is_some() {
+                " by:\n"
+            } else {
+                "!"
+            }
+        ),
+    ];
+
+    let authors = if let Some(authors) = project_authors {
+        authors
+            .iter()
+            .map(|aut| format!("- {}", aut))
+            .collect::<Vec<_>>()
+            .join("\n")
+    } else {
+        String::from("")
+    };
+    begin.push(authors);
+    begin.join("\n")
 }
