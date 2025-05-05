fc02a21bc3e2efd071b95577336666855e9e5e66
NASCC <pedro.main.tec@gmail.com>
2025-05-05 15:24:32 -0300

    feat(report message): store message generator into a sep. mod

---
+++ src/essentials/report_messages.rs
@@ -0,0 +1,24 @@
+use crate::utils::string::StringTransformation;
+use colored::Colorize;
+
+pub fn invalid_project_name<T>(name: T) -> String
+where
+    T: AsRef<str>,
+{
+    let name = name.as_ref();
+    let fixed_name = name.to_valid_camel_case();
+
+    format!(
+        "\
+        The `{}` project name isn't allowed!\n\n\
+        The project name should be {} and have no {}.\n\
+        {}{} numeric start isn't allowed too\n\n\
+        Consider using `{}` instead!",
+        name.bright_red(),
+        "Camel Case".bright_cyan(),
+        "Special Chars".bright_cyan(),
+        "note".bright_cyan(),
+        ":".bright_white(),
+        fixed_name.bright_green()
+    )
+}
