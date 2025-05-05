5e53cfed2b8dae56066a2172163bc55965ee4f31
NASCC <pedro.main.tec@gmail.com>
2025-05-05 17:04:11 -0300

    fix: import from type submodule

--- src/kojamp/app_factory.rs
+++ src/kojamp/app_factory.rs
@@ -1,6 +1,6 @@
 use super::action;
 use crate::{
-    essentials::{KojampReport, ReportType},
+    essentials::report_types::{KojampReport, ReportType},
     globals::{FAILURE_EXIT_STATUS, PROGRAM_REPO_URL, SUCCESS_EXIT_STATUS},
     utils::array::ToText,
 };
