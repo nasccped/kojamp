3bf5e30bbe0ede84c1576e162adb23c349d1a256
NASCC <pedro.main.tec@gmail.com>
2025-05-05 15:26:49 -0300

    feat(string conversion): convert string into a valid camel case

--- src/utils/string.rs
+++ src/utils/string.rs
@@ -2,6 +2,7 @@ use regex::Regex;
 
 pub trait StringTransformation {
     fn to_kebab_case(self) -> String;
+    fn to_valid_camel_case(self) -> String;
 }
 
 impl<T: AsRef<str>> StringTransformation for T {
@@ -17,5 +18,34 @@ impl<T: AsRef<str>> StringTransformation for T {
         let step3 = re3.replace_all(&step2, "-").to_string();
 
         step3.trim_matches('-').to_ascii_lowercase()
+    }
+
+    fn to_valid_camel_case(self) -> String {
+        let valid_radix: String = self
+            .as_ref()
+            .chars()
+            .map(|c| if c.is_digit(36) { c } else { ' ' })
+            .collect();
+
+        let camel_case: String = valid_radix
+            .split_whitespace()
+            .map(|word| word[..1].to_uppercase() + word[1..].to_lowercase().as_str())
+            .collect();
+
+        let early_returning_conditions = [
+            camel_case.is_empty(),
+            camel_case.chars().all(|c| ('0'..='9').contains(&c)),
+        ];
+
+        if early_returning_conditions.iter().any(|&c| c) {
+            return "SomeCoolProject".into();
+        }
+
+        let start_index: usize = camel_case
+            .chars()
+            .position(|c| !('0'..='9').contains(&c))
+            .unwrap_or(0);
+
+        camel_case[start_index..].into()
     }
 }
