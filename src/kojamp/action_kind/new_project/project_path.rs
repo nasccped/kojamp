71fed3063995941ef4b7680578b0ada160de9965
NASCC <pedro.main.tec@gmail.com>
2025-05-08 20:17:03 -0300

    chore + refactor: reduce functions with `?` oper. + get absolute path

--- src/kojamp/action_kind/new_project/project_path.rs
+++ src/kojamp/action_kind/new_project/project_path.rs
@@ -14,63 +14,56 @@ pub struct ProjectPath {
 
 impl ProjectPath {
     pub fn try_new() -> Result<Self, ()> {
-        match env::current_dir() {
-            Ok(x) => Ok(Self {
-                current_path: x,
-                specified_path: None,
-            }),
-            _ => Err(()),
-        }
+        let curdir = env::current_dir().map_err(|_| ())?;
+
+        Ok(Self {
+            current_path: curdir,
+            specified_path: None,
+        })
     }
 
     pub fn is_valid(&self, should_exists: bool) -> bool {
+        let path = if let Some(x) = self.current_path.file_name() {
+            x
+        } else {
+            return false;
+        };
+
+        if path == Path::new("") {
+            return false;
+        }
+
+        self.get_absolute_path().exists() == should_exists
+    }
+
+    pub fn get_absolute_path(&self) -> PathBuf {
         let mut path = self.current_path.clone();
 
-        if path.file_name().is_none() {
-            return false;
+        if let Some(s) = &self.specified_path {
+            path.push(s.as_ref());
         }
 
-        let parent = path.parent();
-
-        if parent.is_none() {
-            return false;
-        }
-
-        let parent = parent.unwrap();
-
-        if !parent.exists() {
-            return false;
-        }
-
-        if parent == Path::new("") {
-            return false;
-        }
-
-        if let Some(spc_pth) = self.specified_path.as_ref() {
-            path.push(spc_pth.as_ref());
-        }
-
-        path.exists() == should_exists
+        path
     }
 
     pub fn add_from_matching(&mut self, matching: &ArgMatches) -> Option<()> {
-        if let Some(path) = matching.get_one::<String>("path") {
-            self.specified_path = Some(Rc::from(path.as_ref()));
-            Some(())
-        } else {
-            None
-        }
+        let specified = matching.get_one::<String>("path")?;
+        self.specified_path = Some(Rc::from(specified.as_ref()));
+        Some(())
     }
 
     pub fn add_from_project_name(&mut self, project_name: &ProjectName) {
-        self.specified_path = Some(Rc::from(project_name.as_str().to_kebab_case()))
+        let value = project_name.as_str().to_kebab_case();
+        self.specified_path = Some(Rc::from(value))
     }
 
     pub fn get_inner(&self) -> PathBuf {
-        let path = match &self.specified_path {
-            Some(x) => x.as_ref(),
-            _ => ".",
-        };
+        let path = self
+            .specified_path
+            .as_ref()
+            .map(|x| x.as_ref())
+            .unwrap_or(".");
+
         PathBuf::from(path)
     }
 }
