e5857570e6747c50f265f2e45aec38b2ae796448
NASCC <pedro.main.tec@gmail.com>
2025-05-08 23:02:04 -0300

    feat: add util for message mod

---
+++ src/essentials/report/util.rs
@@ -0,0 +1,14 @@
+use colored::Colorize;
+
+pub trait IntoReasons {
+    fn into_reasons(self) -> impl Iterator<Item = String>;
+}
+
+impl<const N: usize> IntoReasons for [String; N] {
+    fn into_reasons(self) -> impl Iterator<Item = String> {
+        self.into_iter()
+            .zip('a'..='z')
+            .map(|(ind, r)| format!("... {} {}", (ind.to_string() + ")").bright_cyan(), r))
+            .into_iter()
+    }
+}
