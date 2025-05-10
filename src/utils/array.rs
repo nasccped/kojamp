dcf3b71e46b0b781a6ba66315bf66c7f6dd97f8b
NASCC <pedro.main.tec@gmail.com>
2025-05-10 11:22:25 -0300

    fix: remove array mod (no longer necessary)

--- src/utils/array.rs
+++
@@ -1,9 +0,0 @@
-pub trait ToText {
-    fn to_text(&self) -> &'static str;
-}
-
-impl<const N: usize> ToText for [&str; N] {
-    fn to_text(&self) -> &'static str {
-        Box::leak(self.join("\n").replace("&&\n", "").into_boxed_str())
-    }
-}
