2905b774b5e1d953f80b04bb80b51a4d2c20c709
NASCC <pedro.main.tec@gmail.com>
2025-05-12 11:58:25 -0300

    chore: remove 'on update' warning

--- src/kojamp/subcommands/init.rs
+++ src/kojamp/subcommands/init.rs
@@ -1,43 +1,40 @@
 use clap::{Arg, ArgAction, Command};
-use colored::Colorize;
 
 pub fn cmd() -> Command {
-    let description = format!(
-        "Create a Java/Kotlin project in the current directory {}",
-        "(Still being developed)".bright_red()
-    );
-
-    Command::new("init").alias("ini").about(description).args([
-        Arg::new("name")
-            .value_name("CAMEL_CASED_NAME")
-            .action(ArgAction::Set)
-            .required(true)
-            .help("Specifies the project name")
-            .index(1),
-        Arg::new("kind")
-            .long("kind")
-            .short('K')
-            .required(false)
-            .value_name("JAVA|KOTLIN")
-            .action(ArgAction::Set)
-            .help("Specifies the project kind"),
-        Arg::new("authors")
-            .long("authors")
-            .short('A')
-            .required(false)
-            .value_name("QUOTED_LIST")
-            .action(ArgAction::Set)
-            .help("Specifies the project authors"),
-        Arg::new("no-git")
-            .long("no-git")
-            .required(false)
-            .action(ArgAction::SetFalse)
-            .help("No git repo initialization when starting new project"),
-        Arg::new("force")
-            .long("force")
-            .short('F')
-            .required(false)
-            .action(ArgAction::SetTrue)
-            .help("Force project initialization in a non empty dir"),
-    ])
+    Command::new("init")
+        .alias("ini")
+        .about("Create a Java/Kotlin project in the current directory")
+        .args([
+            Arg::new("name")
+                .value_name("CAMEL_CASED_NAME")
+                .action(ArgAction::Set)
+                .required(true)
+                .help("Specifies the project name")
+                .index(1),
+            Arg::new("kind")
+                .long("kind")
+                .short('K')
+                .required(false)
+                .value_name("JAVA|KOTLIN")
+                .action(ArgAction::Set)
+                .help("Specifies the project kind"),
+            Arg::new("authors")
+                .long("authors")
+                .short('A')
+                .required(false)
+                .value_name("QUOTED_LIST")
+                .action(ArgAction::Set)
+                .help("Specifies the project authors"),
+            Arg::new("no-git")
+                .long("no-git")
+                .required(false)
+                .action(ArgAction::SetFalse)
+                .help("No git repo initialization when starting new project"),
+            Arg::new("force")
+                .long("force")
+                .short('F')
+                .required(false)
+                .action(ArgAction::SetTrue)
+                .help("Force project initialization in a non empty dir"),
+        ])
 }
