e72b35024fc0d1c404b859f0701569bc4f72e56c
NASCC <pedro.main.tec@gmail.com>
2025-05-16 12:48:20 -0300

    fix: separated main content from visual

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,9 +1,17 @@
-import panel
+def print_banner():
+    from panel import banner
+    print()
+    banner.banner()
+    print()
 
 def main():
-    print()
-    panel.banner()
-    print()
+    from models import project
+
+    project = project.Project()
+    err = project.unwrap_err()
+    if err is not None:
+        err.raise_err()
 
 if __name__ == "__main__":
+    print_banner()
     main()
