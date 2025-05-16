e26d060c89a61d2d3e5223c9a19f537cb6fa24a2
NASCC <pedro.main.tec@gmail.com>
2025-05-15 21:52:15 -0300

    chore: use `visible_alias` for each subcommand (except `new`)

--- src/kojamp/subcommands/build.rs
+++ src/kojamp/subcommands/build.rs
@@ -3,6 +3,6 @@ use colored::Colorize;
 
 pub fn cmd() -> Command {
     Command::new("build")
-        .alias("b")
+        .visible_alias("b")
         .about("Being developed".bright_red().to_string())
 }
