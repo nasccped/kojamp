e25152088d22245aa41f1b6619afc83bc717b141
NASCC <pedro.main.tec@gmail.com>
2025-05-18 10:30:47 -0300

    fix: move visual alerts to visual module

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -5,7 +5,7 @@ It should be ran in the repository root path.
 You don't need to specify the file entry. Just use
 `python <PATH_NAME> (scripts for this case)`
 """
-from colors import CYAN_NONE, RESET_ESCAPE, RED_NONE, GREEN_NONE
+from colors import RESET_ESCAPE, GREEN_NONE
 from core.env import CRATE_NAME, IMAGE_NAME
 from core.target_files import CARGO_TOML
 from error_types.base_error import BaseError
@@ -13,16 +13,9 @@ from models.crates_io_bridge import CratesIOBridge
 from models.dockerhub_bridge import DockerHubBridge
 from models.file import File
 from models.project import Project
+from visual.alerts import waiting_alert
 from visual.banner import program_begin, program_errors
 from visual.screen import clear
-
-def waiting_alert():
-    cyan_files = CYAN_NONE + "files" + RESET_ESCAPE
-    cyan_urls = CYAN_NONE + "urls" + RESET_ESCAPE
-    take_a_while = RED_NONE + "take a while" + RESET_ESCAPE
-
-    print(f"Fetching data from {cyan_files} and {cyan_urls}!")
-    print(f"This can {take_a_while}...")
 
 def load_models():
     global docker_bridge, crates_bridge, cargo_file
