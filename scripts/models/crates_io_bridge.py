ae8cb8169d4e032f35ad9980e250f0b12b7f233f
NASCC <pedro.main.tec@gmail.com>
2025-05-18 10:19:17 -0300

    chore: add new model (CratesIOBridge)

---
+++ scripts/models/crates_io_bridge.py
@@ -0,0 +1,65 @@
+import json
+import base64
+from core.urls import CRATES_IO_URL
+from error_types.derived_errors import UnfetchableURL
+import requests
+
+def get_versions_from_cratesio_ghub(target_crate: str) -> list[str] | UnfetchableURL:
+    contents_url = CRATES_IO_URL + "/contents"
+    url_parts = [target_crate[i : i + 2] for i in range(0, len(target_crate), 2)]
+    conn = None
+
+    while True:
+        if len(url_parts) == 0:
+            return UnfetchableURL(contents_url)
+
+        path_url = "/".join(url_parts[:-1])
+        full_crate_url = "/".join([contents_url, path_url, target_crate])
+
+        conn = requests.get(full_crate_url)
+
+        if conn.status_code == 200:
+            break
+
+        if len(url_parts[-1]) > 1:
+            url_parts[-1] = url_parts[-1][:-1]
+        else:
+            url_parts = url_parts[:-1]
+
+    content = conn.json()["content"]
+    decoded_bytes = base64.b64decode(content)
+    data_rows = decoded_bytes.decode("utf-8").split("\n")
+
+    versions = []
+
+    for row in data_rows:
+        if len(row) == 0:
+            continue
+        as_json = json.loads(row)
+        versions.append(as_json["vers"])
+
+    if len(versions) == 0:
+        return UnfetchableURL(full_crate_url)
+
+    return versions
+
+class CratesIOBridge:
+    """
+    Store the project info related fields (latest version) from
+    crates.io
+    """
+
+    def __init__(self, crate_name: str) -> None:
+        versions = get_versions_from_cratesio_ghub(crate_name)
+        error = None
+
+        if isinstance(versions, UnfetchableURL):
+            error = versions
+            versions = None
+
+        self.crate_name = crate_name
+        self.versions = versions
+        self.error = error
+
+    def unwrap_err(self) -> None | UnfetchableURL:
+        return self.error
