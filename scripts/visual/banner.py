5e44b839393fb89298beb4d2f889cc73712018cd
NASCC <pedro.main.tec@gmail.com>
2025-05-19 20:48:06 -0300

    chore: send same responsability to a single function

--- scripts/visual/banner.py
+++ scripts/visual/banner.py
@@ -1,19 +1,16 @@
-def program_begin():
-    from colors import (WHITE_NONE, GREEN_NONE, RESET_ESCAPE)
-    title = "Kojamp Publish Script"
+from colors import WHITE_NONE, GREEN_NONE, RESET_ESCAPE, RED_NONE
 
+def draw_title(title: str, escape: str):
     print()
     print(f"{WHITE_NONE}{'=' * 70}{RESET_ESCAPE}")
-    print(f"{GREEN_NONE}{title.center(70)}{RESET_ESCAPE}")
+    print(f"{escape}{title.center(70)}{RESET_ESCAPE}")
     print(f"{WHITE_NONE}{'=' * 70}{RESET_ESCAPE}")
     print()
+
+def program_begin():
+    title = "Kojamp Publish Script"
+    draw_title(title, GREEN_NONE)
 
 def program_errors():
-    from colors import (WHITE_NONE, RED_NONE, RESET_ESCAPE)
     title = "Errors"
-
-    print()
-    print(f"{WHITE_NONE}{'=' * 70}{RESET_ESCAPE}")
-    print(f"{RED_NONE}{title.center(70)}{RESET_ESCAPE}")
-    print(f"{WHITE_NONE}{'=' * 70}{RESET_ESCAPE}")
-    print()
+    draw_title(title, RED_NONE)
