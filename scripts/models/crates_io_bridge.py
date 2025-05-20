2e7665933608085e2d9d41b4b2d694ebedde479f
NASCC <pedro.main.tec@gmail.com>
2025-05-19 22:56:40 -0300

    chore: get `ProgramVersion` object instead of `list[str]`

--- scripts/models/crates_io_bridge.py
+++ scripts/models/crates_io_bridge.py
@@ -1,48 +1,91 @@
 import json
 import base64
 from core.urls import CRATES_IO_URL
+from core.env import CRATE_NAME
 from error_types.derived_errors import UnfetchableURL
+from error_types.derived_errors import UnfetchableDataFile
 from models.program_version import ProgramVersion
 import requests
 
-def get_versions_from_cratesio_ghub(target_crate: str) -> list[str] | UnfetchableURL:
+def get_crateghub_url(target_crate: str) -> str | UnfetchableURL:
+    """
+    All Rust's crates are disposed at crates.io url and you can
+    access it through an url (getting a json).
+
+    To get the full path of a given crate, you'll need to find
+    it by navigation by a url tree (nested directories).
+
+    This function will try to build the correct url path to a given
+    crate by using brute force. Will return an `UnfetchableURL` if
+    no url found.
+    """
+    # crate url is built like:
+    # somecrate => https://.../contents/so/me/somecrate
+    # basically, the fields before the crate name will be
+    # the crate chars separated by pairs ('so', 'me', '..')
     contents_url = CRATES_IO_URL + "/contents"
-    url_parts = [target_crate[i : i + 2] for i in range(0, len(target_crate), 2)]
-    conn = None
+    url_parts = [
+        target_crate[i : i + 2] \
+            for i in range(0, len(target_crate), 2)
+    ]
 
-    while True:
-        if len(url_parts) == 0:
-            return UnfetchableURL(contents_url)
-
-        path_url = "/".join(url_parts[:-1])
-        full_crate_url = "/".join([contents_url, path_url, target_crate])
-
+    # while there's items within url parts
+    while len(url_parts) > 0:
+        path_url = "/".join(url_parts)
+        full_crate_url = "/".join(
+            [contents_url, path_url, target_crate]
+        )
         conn = requests.get(full_crate_url)
 
+        # if url returned OK
         if conn.status_code == 200:
-            break
+            return full_crate_url
 
         if len(url_parts[-1]) > 1:
             url_parts[-1] = url_parts[-1][:-1]
-        else:
-            url_parts = url_parts[:-1]
-
-    content = conn.json()["content"]
-    decoded_bytes = base64.b64decode(content)
-    data_rows = decoded_bytes.decode("utf-8").split("\n")
-
-    versions = []
-
-    for row in data_rows:
-        if len(row) == 0:
             continue
-        as_json = json.loads(row)
-        versions.append(as_json["vers"])
 
-    if len(versions) == 0:
-        return UnfetchableURL(full_crate_url)
+        url_parts.pop()
 
-    return versions
+    # else (no ok url)
+    return UnfetchableURL(contents_url)
+
+def get_versionlist_from_crateghub_url(
+    url: str
+) -> list[str] | UnfetchableURL | UnfetchableDataFile:
+    """
+    Extract `list[str]` of an crates.io json content.
+
+    Will return `UnfetchableURL` if invalid url. Can also return
+    `UnfetchableDataFile` if the target field doesn't exists
+    """
+    conn = requests.get(url)
+    target_conn_field = "content"
+    target_json_field = "vers"
+    target_encoding = "utf-8"
+
+    if conn.status_code != 200:
+        return UnfetchableURL(url)
+
+    # it comes encoded in base64. Let's decode it to utf-8
+    encoded = conn.json()[target_conn_field]
+    decoded = base64.b64decode(encoded)
+    data_rows = [
+        json.loads(row) \
+            for row in decoded.decode(target_encoding).split("\n") \
+            if len(row) > 0
+    ]
+
+    # use try to avoid FieldDoesntExists error like
+    try:
+        returnable = [row[target_json_field] for row in data_rows]
+    except:
+        returnable = UnfetchableDataFile(
+            f"{url}[\"{target_conn_field}\"]",
+            f"{{ {target_json_field} }}"
+        )
+
+    return returnable
 
 class CratesIOBridge:
     """
@@ -51,19 +94,31 @@ class CratesIOBridge:
     """
 
     def __init__(self, crate_name: str) -> None:
-        versions = get_versions_from_cratesio_ghub(crate_name)
+        final_url = get_crateghub_url(CRATE_NAME)
+        versions = None
 
-        if isinstance(versions, UnfetchableURL):
-            latest = None
-            error = versions
-        else:
-            versions.sort()
-            latest = ProgramVersion(versions[-1])
-            error = None
+        self.crate_name = crate_name
+        self.latest = None
+        self.error = None
 
-        self.crate_name: str = crate_name
-        self.latest = latest
-        self.error = error
+        # if url catching returned an error, update error field and
+        # stop construction
+        if isinstance(final_url, UnfetchableURL):
+            self.error = final_url
+            return
 
-    def unwrap_err(self) -> None | UnfetchableURL:
+        # extract version list
+        result = get_versionlist_from_crateghub_url(final_url)
+
+        # if is error
+        if isinstance(result, UnfetchableURL | UnfetchableDataFile):
+            self.error = result
+            return
+
+        # update `latest` field
+        versions = [ProgramVersion(v) for v in result]
+        versions.sort()
+        self.latest = versions[-1]
+
+    def unwrap_err(self) -> None | UnfetchableURL | UnfetchableDataFile:
         return self.error
