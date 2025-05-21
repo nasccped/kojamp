4fd8d7d7a3346394b5a6c5971fd99743d6b3f0d9
NASCC <pedro.main.tec@gmail.com>
2025-05-21 10:45:47 -0300

    chore: add remote repo consts

--- scripts/core/env.py
+++ scripts/core/env.py
@@ -1,3 +1,4 @@
 IMAGE_NAME = "nasccped/kojamp"
+REMOTE_REPOSITORY = "nasccped/kojamp"
 CRATE_NAME = "kojamp"
 GITHUB_TOKEN_PATH = "scripts/.env/GITHUB_TOKEN.txt"
--- scripts/core/urls.py
+++ scripts/core/urls.py
@@ -1,2 +1,3 @@
 DOCKERHUB_URL = "https://hub.docker.com/v2/repositories"
+GITHUB_API_URL = "https://api.github.com/repos"
 CRATES_IO_URL = "https://api.github.com/repos/rust-lang/crates.io-index"
