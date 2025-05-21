ba402127a0e4629ce2e8c1a8df6e47f706523263
NASCC <pedro.main.tec@gmail.com>
2025-05-21 10:39:34 -0300

    chore: log status code also

--- scripts/error_types/derived_errors/unfetchable_url.py
+++ scripts/error_types/derived_errors/unfetchable_url.py
@@ -7,15 +7,13 @@ class UnfetchableURL(BaseError):
     returns an error (`status_code` != 200)
     """
 
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, stts_code: int) -> None:
         red_url = RED_NONE + url + RESET_ESCAPE
-        cyan_200 = CYAN_NONE + "200" + RESET_ESCAPE
-        not_exists = RED_NONE + "not exists" + RESET_ESCAPE
+        status_code = CYAN_NONE + str(stts_code) + RESET_ESCAPE
         message = [
             f"Couldn't fetch data from the",
             f"'{red_url}' url!",
             "",
-            f"Probably a non {cyan_200} status code has been returned."
-            f"The target data can also {not_exists}!"
+            f"Returned status code {status_code}."
         ]
         super().__init__(message, 1)
