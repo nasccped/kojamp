2a433556efe0bce68e94a0ae350b738056496cfc
NASCC <pedro.main.tec@gmail.com>
2025-05-21 19:42:05 -0300

    chore: add new color (WHITE_BLUE)

--- scripts/colors/__init__.py
+++ scripts/colors/__init__.py
@@ -4,7 +4,8 @@ from .core import \
     GREEN_NONE,   \
     WHITE_NONE,   \
     CYAN_NONE,    \
-    YELLOW_NONE
+    YELLOW_NONE,  \
+    WHITE_BLUE
 
 def apply(message, color_escape: str) -> str:
     return color_escape + message + RESET_ESCAPE
