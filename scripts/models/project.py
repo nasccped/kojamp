59b6b9569f5eae548d300f4a318c7dd0d3cd540a
NASCC <pedro.main.tec@gmail.com>
2025-05-21 11:51:53 -0300

    chore/fix: add param objs to inner fields + new feat (version printing)

--- scripts/models/project.py
+++ scripts/models/project.py
@@ -1,7 +1,9 @@
+from colors import GREEN_NONE, RESET_ESCAPE
 from error_types.base_error import BaseError
 from models.file import File
 from models.dockerhub_bridge import DockerHubBridge
 from models.crates_io_bridge import CratesIOBridge
+from models.local_git_bridge import LocalGitBridge
 from models.remote_git_bridge import RemoteGitBridge
 
 class Project:
@@ -28,14 +30,16 @@ class Project:
         file: File,
         dockerhub_bridge: DockerHubBridge,
         crates_io_bridge: CratesIOBridge,
-        r_git_bridge: RemoteGitBridge
+        r_git_bridge: RemoteGitBridge,
+        l_git_bridge: LocalGitBridge
     ) -> None:
         # unwrap errors in a list
         errors = [
             file.unwrap_err(),
             dockerhub_bridge.unwrap_err(),
             crates_io_bridge.unwrap_err(),
-            r_git_bridge.unwrap_err()
+            r_git_bridge.unwrap_err(),
+            l_git_bridge.unwrap_err()
         ]
         # filter only when not None
         errors = [e for e in errors if e]
@@ -47,7 +51,24 @@ class Project:
         self.file = file
         self.dhub = dockerhub_bridge
         self.crate = crates_io_bridge
+        self.remote = r_git_bridge
+        self.local = l_git_bridge
         self.errors = errors
 
     def get_error_list(self) -> None | list[BaseError]:
         return self.errors
+
+    def print_versions(self):
+        versions = [
+            v.__str__(
+                GREEN_NONE + c.__class__.__name__ + RESET_ESCAPE
+            ) for (v, c) in [
+                (self.file.version, self.file),
+                (self.dhub.latest, self.dhub),
+                (self.crate.latest, self.crate),
+                (self.remote.latest, self.remote),
+                (self.local.latest, self.local)
+            ]
+        ]
+
+        print("\n".join(versions))
