5f8e2bbc8ae0d05c374cef7e6d58a5e1ba5345d5
NASCC <pedro.main.tec@gmail.com>
2025-05-21 16:30:38 -0300

    chore: use color.apply in alerts also

--- scripts/visual/alerts.py
+++ scripts/visual/alerts.py
@@ -1,9 +1,9 @@
-from colors import CYAN_NONE, RESET_ESCAPE, RED_NONE, GREEN_NONE
+from colors import apply, CYAN_NONE, RED_NONE, GREEN_NONE
 
 def waiting_alert():
-    cyan_files = CYAN_NONE + "files" + RESET_ESCAPE
-    cyan_urls = CYAN_NONE + "urls" + RESET_ESCAPE
-    take_a_while = RED_NONE + "take a while" + RESET_ESCAPE
+    cyan_files = apply("files", CYAN_NONE)
+    cyan_urls = apply("urls", CYAN_NONE)
+    take_a_while = apply("take a while", RED_NONE)
 
     print()
     print(f"Fetching data from {cyan_files} and {cyan_urls}!")
@@ -13,18 +13,18 @@ def waiting_alert():
 def local_tag_should_be_greater_than_remote(
     local_tag: str, remote_tag: str
 ):
-    green_local_tag = GREEN_NONE + local_tag + RESET_ESCAPE
-    green_remote_tag = GREEN_NONE + remote_tag + RESET_ESCAPE
-    red_greater = RED_NONE + "greater" + RESET_ESCAPE
+    green_local_tag = apply(local_tag, GREEN_NONE)
+    green_remote_tag = apply(remote_tag, GREEN_NONE)
+    red_greater = apply("greater", RED_NONE)
     print(f"The local git tag is {green_local_tag} but the")
     print(f"remote is {green_remote_tag}. To proceed, the local tag")
     print(f"should be {red_greater}!")
 
 def local_tag_conflict(git_tag: str, file_version: str):
-    green_local_tag = GREEN_NONE + git_tag + RESET_ESCAPE
-    green_file_version = GREEN_NONE + file_version + RESET_ESCAPE
-    red_same = RED_NONE + "same" + RESET_ESCAPE
-    file_version = GREEN_NONE + "file[version]" + RESET_ESCAPE
+    green_local_tag = apply(git_tag, GREEN_NONE)
+    green_file_version = apply(file_version, GREEN_NONE)
+    red_same = apply("same", RED_NONE)
+    file_version = apply("file[version]", GREEN_NONE)
     print(f"The local git tag is {green_local_tag} but the")
     print(f"file version is {green_file_version}. To proceed, the")
     print(f"local git tag should be the {red_same} as the")
