7140c7cbfbfaf25f20176bec7de1d0aa7a607016
NASCC <pedro.main.tec@gmail.com>
2025-05-22 18:13:19 -0300

    chore: change the todo result to a reserved place

--- src/kojamp/action_kind/build_project/main.rs
+++ src/kojamp/action_kind/build_project/main.rs
@@ -2,12 +2,17 @@ use clap::ArgMatches;
 
 use crate::core::reporting::{KojampReport, ReportType};
 
-pub fn main(matching: ArgMatches) -> Result<Vec<KojampReport>, Vec<KojampReport>> {
-    let _matching = &matching;
-    // TODO: implement logic here
-    Err(vec![KojampReport::new(
+fn todo_result() -> Vec<KojampReport> {
+    vec![KojampReport::new(
         ReportType::Error,
         "Not yet implemented",
         "The code to build project should be implemented",
-    )])
+    )]
+}
+
+pub fn main(matching: ArgMatches) -> Result<Vec<KojampReport>, Vec<KojampReport>> {
+    let _matching = &matching;
+    // TODO: implement logic here
+
+    Err(todo_result())
 }
