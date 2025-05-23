26d66b7d6136073b71d539907c233739ad43800d
NASCC <pedro.main.tec@gmail.com>
2025-05-23 20:06:18 -0300

    chore: proceed with latest depend. changes/updates

--- src/kojamp/action_kind/build_project/main.rs
+++ src/kojamp/action_kind/build_project/main.rs
@@ -1,6 +1,7 @@
 use super::{reports::*, utils::*};
 use crate::core::reporting::{KojampReport, ReportType};
 use clap::ArgMatches;
+use std::fs;
 
 fn todo_result() -> Vec<KojampReport> {
     vec![KojampReport::new(
@@ -11,16 +12,29 @@ fn todo_result() -> Vec<KojampReport> {
 }
 
 const KOJAMP_TOML: &str = "Kojamp.toml";
+const SRC_DIR: &str = "src";
 
 pub fn main(matching: ArgMatches) -> Result<Vec<KojampReport>, Vec<KojampReport>> {
     let _matching = &matching;
     let curdir = std::env::current_dir().map_err(|_| vec![could_not_get_curdir()])?;
 
-    match file_is_here(&curdir, KOJAMP_TOML) {
-        Ok(false) => return Err(vec![kojamp_toml_is_missing()]),
-        Err(_) => return Err(vec![could_not_read_dir()]),
-        _ => {}
-    }
+    match item_is_here(&curdir, KOJAMP_TOML) {
+        Ok(false) => Err(vec![kojamp_toml_is_missing()]),
+        Err(_) => Err(vec![could_not_read_dir()]),
+        _ => Ok(()),
+    }?;
 
+    match item_is_here(&curdir, SRC_DIR) {
+        Ok(false) => Err(vec![src_dir_is_missing()]),
+        Err(_) => Err(vec![could_not_read_dir()]),
+        _ => Ok(()),
+    }?;
+
+    let toml_content = fs::read_to_string(curdir.join(KOJAMP_TOML))
+        .map_err(|_| vec![could_not_read_toml_file()])?;
+
+    let _name = get_project_name_from_toml(toml_content).unwrap();
+
+    // TODO: implement the remaining logic
     Err(todo_result())
 }
