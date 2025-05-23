b3bba540976ef5319742d51bf92c65676eec3408
NASCC <pedro.main.tec@gmail.com>
2025-05-23 16:06:16 -0300

    chore: use build action reports + utils

--- src/kojamp/action_kind/build_project/main.rs
+++ src/kojamp/action_kind/build_project/main.rs
@@ -1,6 +1,6 @@
-use clap::ArgMatches;
-
+use super::{reports::*, utils::*};
 use crate::core::reporting::{KojampReport, ReportType};
+use clap::ArgMatches;
 
 fn todo_result() -> Vec<KojampReport> {
     vec![KojampReport::new(
@@ -10,9 +10,17 @@ fn todo_result() -> Vec<KojampReport> {
     )]
 }
 
+const KOJAMP_TOML: &str = "Kojamp.toml";
+
 pub fn main(matching: ArgMatches) -> Result<Vec<KojampReport>, Vec<KojampReport>> {
     let _matching = &matching;
-    // TODO: implement logic here
+    let curdir = std::env::current_dir().map_err(|_| vec![could_not_get_curdir()])?;
+
+    match file_is_here(&curdir, KOJAMP_TOML) {
+        Ok(false) => return Err(vec![kojamp_toml_is_missing()]),
+        Err(_) => return Err(vec![could_not_read_dir()]),
+        _ => {}
+    }
 
     Err(todo_result())
 }
