216755375b70acdb675e895e280597ee44afa89f
NASCC <pedro.main.tec@gmail.com>
2025-05-23 20:03:59 -0300

    chore/fix: get data from toml (proj. name) + change func name

--- src/kojamp/action_kind/build_project/utils.rs
+++ src/kojamp/action_kind/build_project/utils.rs
@@ -1,6 +1,6 @@
 use std::{fs, path::PathBuf};
 
-pub fn file_is_here<T: AsRef<str>>(path: &PathBuf, file: T) -> Result<bool, ()> {
+pub fn item_is_here<T: AsRef<str>>(path: &PathBuf, file: T) -> Result<bool, ()> {
     Ok(fs::read_dir(path)
         .map_err(|_| ())?
         .filter_map(|entry| entry.ok())
@@ -11,4 +11,10 @@ pub fn file_is_here<T: AsRef<str>>(path: &PathBuf, file: T) -> Result<bool, ()> 
                 .map(|name| name == file.as_ref())
                 .unwrap_or(false)
         }))
+}
+
+pub fn get_project_name_from_toml(file_input: String) -> Result<String, ()> {
+    let toml_file = file_input.parse::<toml::Value>().map_err(|_| ())?;
+    let name = toml_file["project"]["name"].as_str().ok_or(())?;
+    Ok(String::from(name))
 }
