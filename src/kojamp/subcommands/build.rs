bb53c438466a22148077efd847a88afe41c95ccc
NASCC <pedro.main.tec@gmail.com>
2025-05-27 11:55:57 -0300

    chore: remove unnecessary (not used) arg

--- src/kojamp/subcommands/build.rs
+++ src/kojamp/subcommands/build.rs
@@ -1,13 +1,7 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Command;
 
 pub fn cmd() -> Command {
     Command::new("build")
         .visible_alias("b")
         .about("Build the project bytecode (.class/.jar)")
-        .args([Arg::new("polished")
-            .long("polished")
-            .short('p')
-            .action(ArgAction::SetTrue)
-            .required(false)
-            .help("Build only the changed source, preserving the old .class(es)")])
 }
