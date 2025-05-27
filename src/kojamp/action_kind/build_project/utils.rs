c966e0104dfb73c44f36beed44ddcb467b18d398
NASCC <pedro.main.tec@gmail.com>
2025-05-27 17:45:47 -0300

    chore: get all file sources

--- src/kojamp/action_kind/build_project/utils.rs
+++ src/kojamp/action_kind/build_project/utils.rs
@@ -1,4 +1,7 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 pub fn item_is_here<T: AsRef<str>>(path: &PathBuf, file: T) -> Result<bool, ()> {
     Ok(fs::read_dir(path)
@@ -25,4 +28,33 @@ pub fn get_project_kind_from_toml(file_input: &str) -> Result<String, ()> {
     let project_table = binding.get("project").ok_or(())?;
     let kind_field = project_table.get("kind").ok_or(())?.as_str().ok_or(())?;
     Ok(String::from(kind_field))
+}
+
+pub fn get_all_sources<T: AsRef<str>>(kind: T, init_path: &Path) -> Result<Vec<PathBuf>, PathBuf> {
+    let ext_target = match kind.as_ref() {
+        "java" => "java",
+        _ => "kt",
+    };
+    let mut output = Vec::new();
+
+    let files = init_path.read_dir().map_err(|_| init_path)?;
+
+    for f in files {
+        let entry = f.map_err(|_| PathBuf::from("UNDEFINED???"))?;
+        let abs = entry.path();
+        if abs.is_dir() {
+            let nested_path = abs.file_name().ok_or(&abs)?;
+            let nested_path = init_path.join(nested_path);
+            let nested_files = get_all_sources(kind.as_ref(), &nested_path)?;
+            output.extend(nested_files.into_iter());
+        }
+        let f_name = abs.file_name().ok_or(&abs)?;
+        match f_name.to_str() {
+            None => return Err(PathBuf::from(f_name)),
+            Some(x) if x.ends_with(ext_target) => output.push(init_path.join(x)),
+            _ => {}
+        }
+    }
+
+    Ok(output)
 }
