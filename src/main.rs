ae4425847149b86295b43cde17d573c0a98734ac
Nasccped <pedro.main.tec@gmail.com>
2025-09-07 11:20:51 -0300

    fail: regret workspace (discontinued project)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,14 @@ mod utils;
 
 use kojamp::{kojamp_app, KojampApp};
 
+fn big_warning() {
+    println!("\x1b[93mwarning\x1b[0m: this project is no longer maintained.");
+    println!();
+    println!("JSmoke (https://github.com/nasccped/jsmoke) is/will be the successor!");
+}
+
 fn main() {
+    big_warning();
     let mut app = kojamp_app();
     let matching = app.get_matching();
     let output = app.run_kojamp_app(matching);
